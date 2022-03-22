956006e3ec61f910e28e6f4a8e903d8a9eb32bf1
jfsilva6 <joaofilipevazsilva@gmail.com>
2022-03-22 16:03:38 +0000

    cores

--- src/colorsAdjustment/colorAdjustmentContent.js
+++ src/colorsAdjustment/colorAdjustmentContent.js
@@ -11,3 +11,31 @@ $("#btn_mudaCorConteudo").click(function (e) {
     $("#mudaCorConteudo").show();
   }
 });
+
+$('input:radio[name="editaCor"]').change(function () {
+  if ($(this).val() == "conteudo") {
+    $('input:radio[name="Cor"]').change(function () {
+      if ($(this).val() == "red") {
+        $("body").removeClass("ConteudoAzul");
+        $("body").removeClass("ConteudoAmarelo");
+        $("body").removeClass("ConteudoRosa");
+        $("body").addClass("ConteudoVermelho");
+      } else if ($(this).val() == "blue") {
+        $("body").removeClass("ConteudoVermelho");
+        $("body").removeClass("ConteudoAmarelo");
+        $("body").removeClass("ConteudoRosa");
+        $("body").addClass("ConteudoAzul");
+      } else if ($(this).val() == "yellow") {
+        $("body").removeClass("ConteudoVermelho");
+        $("body").removeClass("ConteudoAzul");
+        $("body").removeClass("ConteudoRosa");
+        $("body").addClass("ConteudoAmarelo");
+      } else if ($(this).val() == "pink") {
+        $("body").removeClass("ConteudoVermelho");
+        $("body").removeClass("ConteudoAzul");
+        $("body").removeClass("ConteudoAmarelo");
+        $("body").addClass("ConteudoRosa");
+      }
+    });
+  }
+});
