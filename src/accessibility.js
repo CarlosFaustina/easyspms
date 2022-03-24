543906ad5fd4fc198cc147f863c4136bb67b41b3
Marcio Mota <mjpinto@primeit.pt>
2022-03-24 16:30:11 +0000

    ajuste no webpack para compilar css

--- src/accessibility.js
+++ src/accessibility.js
@@ -1,5 +1,7 @@
 "use strict";
+import "./sass/main.scss";
 import Accessibility from "./main.js";
+
 
 window.Accessibility = Accessibility;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: "./src/accessibility.js",
   mode: "production",
   output: {
+    filename: "[name].min.js",
     path: path.resolve(__dirname, "api"),
     libraryTarget: "umd",
-    filename: "accessibility.min.js",
     auxiliaryComment: "accessibility output",
   },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
 };
