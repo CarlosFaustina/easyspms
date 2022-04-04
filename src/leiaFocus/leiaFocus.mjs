77d2880ede2b63ec1bfb36a4fe8735851380bd63
CarlosFaustina <101330171+CarlosFaustina@users.noreply.github.com>
2022-04-04 12:45:26 +0100

    leiaFocus02Update

--- src/leiaFocus/leiaFocus.mjs
+++ src/leiaFocus/leiaFocus.mjs
@@ -15,18 +15,17 @@ export default function leiaFocus(self, destroy) {
   document.getElementsByTagName("body")[0].appendChild(divFocus);
   document.getElementsByTagName("body")[0].appendChild(divMascara);
   document.getElementsByTagName("body")[0].appendChild(divMascara2);
-    document.getElementById("divMascara").style.opacity = "10%";
-  document.getElementById("divMascara2").style.opacity = "10%";
 
 
-  document.getElementById("divMascara2").style.transitionDuration = "2s";
-  document.getElementById("divMascara").style.transitionDuration = "2s";
+
+  document.getElementById("divMascara2").style.transitionDuration = "1s";
+  document.getElementById("divMascara").style.transitionDuration = "1s";
   
   document.getElementById("divMascara").style.background = "rgb(95, 95, 95)";
   document.getElementById("divMascara2").style.background = "rgb(95, 95, 95)";
 
-  document.getElementById("divMascara").style.zIndex = "222";
-  document.getElementById("divMascara2").style.zIndex = "222";
+  document.getElementById("divMascara").style.zIndex = "22222";
+  document.getElementById("divMascara2").style.zIndex = "22222";
 
   document.getElementById("divMascara").style.borderBottomLeftRadius =
     10 + "px";
@@ -63,8 +62,8 @@ export default function leiaFocus(self, destroy) {
   window.addEventListener("mousemove", (e) => {
     // cursor.style.left = (e.x-100)+"px";// para poder mover no eixo X.
 
-    // document.getElementById("divMascara").style.transition = "200ms";
-    // document.getElementById("divMascara2").style.transition = "200ms";
+     document.getElementById("divMascara").style.transition = "50ms";
+     document.getElementById("divMascara2").style.transition = "100ms";
 
     //cursor.style.top = (e.y-50)+"px";
 
@@ -75,39 +74,46 @@ export default function leiaFocus(self, destroy) {
   ///
 
   if (destroy) {
-    document
-      .querySelector('._access-menu [data-access-action="leiaFocus"]')
-      .classList.remove("active");
+    
+    document.getElementById("divMascara").style.opacity = "15%";
+    document.getElementById("divMascara2").style.opacity = "15%";
+    document.getElementById("divMascara").style.height = "50%";
+    document.getElementById("divMascara2").style.top = "50%";
+    
+    document.querySelector('._access-menu [data-access-action="leiaFocus"]').classList.remove("active");
     self.initialValues.leiaFocus = false;
     self.sessionState.leiaFocus = false;
     self.onChange(true);
 
     document.getElementById("divMascara").style.visibility = "hidden";
     document.getElementById("divMascara2").style.visibility = "hidden";
-    window.removeEventListener("mousemove"); //gere o erro. falta segundo argumento. Em principio a funcao quale foi chamada para addEventListener
+    window.removeEventListener("mousemove",addEventListener); //gere o erro. 
     return;
   }
 
   document.getElementById("divMascara").style.visibility = "visible";
   document.getElementById("divMascara2").style.visibility = "visible";
 
-  document
-    .querySelector('._access-menu [data-access-action="leiaFocus"]')
-    .classList.toggle("active");
+  document.querySelector('._access-menu [data-access-action="leiaFocus"]').classList.toggle("active");
   self.initialValues.leiaFocus = !self.initialValues.leiaFocus;
   self.sessionState.leiaFocus = self.initialValues.leiaFocus;
 
   if (!self.initialValues.leiaFocus) {
-    document
-      .querySelector('._access-menu [data-access-action="leiaFocus"]')
-      .classList.remove("active");
+    window.removeEventListener("mousemove", e); 
+    
+    document.getElementById("divMascara").style.opacity = "15%";
+    document.getElementById("divMascara2").style.opacity = "15%";
+    document.getElementById("divMascara").style.height = "50%";
+    document.getElementById("divMascara2").style.top = "50%";
+    
+    document.querySelector('._access-menu [data-access-action="leiaFocus"]').classList.remove("active");
     self.initialValues.leiaFocus = false;
     self.sessionState.leiaFocus = false;
     self.onChange(true);
 
     document.getElementById("divMascara").style.visibility = "hidden";
     document.getElementById("divMascara2").style.visibility = "hidden";
-    window.removeEventListener("mousemove");
+    
     return;
   }
   self.onChange(true);
