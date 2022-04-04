a2141a71efdf424b53a532e2e646f303ee2dee74
CarlosFaustina <101330171+CarlosFaustina@users.noreply.github.com>
2022-04-04 12:04:20 +0100

    leiaFocus Update

--- src/leiaFocus/leiaFocus.mjs
+++ src/leiaFocus/leiaFocus.mjs
@@ -15,18 +15,17 @@ export default function leiaFocus(self, destroy) {
   document.getElementsByTagName("body")[0].appendChild(divFocus);
   document.getElementsByTagName("body")[0].appendChild(divMascara);
   document.getElementsByTagName("body")[0].appendChild(divMascara2);
-    document.getElementById("divMascara").style.opacity = "10%";
-  document.getElementById("divMascara2").style.opacity = "10%";
 
 
-  document.getElementById("divMascara2").style.transitionDuration = "2s";
-  document.getElementById("divMascara").style.transitionDuration = "2s";
+
+  document.getElementById("divMascara2").style.transitionDuration = "1s";
+  document.getElementById("divMascara").style.transitionDuration = "1s";
   
   document.getElementById("divMascara").style.background = "rgb(95, 95, 95)";
   document.getElementById("divMascara2").style.background = "rgb(95, 95, 95)";
 
-  document.getElementById("divMascara").style.zIndex = "222";
-  document.getElementById("divMascara2").style.zIndex = "222";
+  document.getElementById("divMascara").style.zIndex = "2222";
+  document.getElementById("divMascara2").style.zIndex = "2222";
 
   document.getElementById("divMascara").style.borderBottomLeftRadius =
     10 + "px";
@@ -63,8 +62,8 @@ export default function leiaFocus(self, destroy) {
   window.addEventListener("mousemove", (e) => {
     // cursor.style.left = (e.x-100)+"px";// para poder mover no eixo X.
 
-    // document.getElementById("divMascara").style.transition = "200ms";
-    // document.getElementById("divMascara2").style.transition = "200ms";
+    // document.getElementById("divMascara").style.transition = "100ms";
+    // document.getElementById("divMascara2").style.transition = "50ms";
 
     //cursor.style.top = (e.y-50)+"px";
 
@@ -75,8 +74,11 @@ export default function leiaFocus(self, destroy) {
   ///
 
   if (destroy) {
-    document
-      .querySelector('._access-menu [data-access-action="leiaFocus"]')
+    
+    document.getElementById("divMascara").style.opacity = "15%";
+    document.getElementById("divMascara2").style.opacity = "15%";
+    
+    document.querySelector('._access-menu [data-access-action="leiaFocus"]')
       .classList.remove("active");
     self.initialValues.leiaFocus = false;
     self.sessionState.leiaFocus = false;
@@ -98,9 +100,11 @@ export default function leiaFocus(self, destroy) {
   self.sessionState.leiaFocus = self.initialValues.leiaFocus;
 
   if (!self.initialValues.leiaFocus) {
-    document
-      .querySelector('._access-menu [data-access-action="leiaFocus"]')
-      .classList.remove("active");
+    
+    document.getElementById("divMascara").style.opacity = "15%";
+    document.getElementById("divMascara2").style.opacity = "15%";
+    
+    document.querySelector('._access-menu [data-access-action="leiaFocus"]').classList.remove("active");
     self.initialValues.leiaFocus = false;
     self.sessionState.leiaFocus = false;
     self.onChange(true);
