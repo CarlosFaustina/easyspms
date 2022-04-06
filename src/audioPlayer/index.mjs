1a0bc5af58369e33c072cb771cf7eb05a4f3201f
Anderson Carlos <dersoncarlos@gmail.com>
2022-04-06 18:35:09 -0300

    player

--- src/audioPlayer/index.mjs
+++ src/audioPlayer/index.mjs
@@ -1,5 +1,4 @@
 export const audioPlayerHtml = `
-<!-- AUDIO PLAYER -->
 <div class="accessibilityPlayerAudioContainer" id="accessibilityAudioPlayer">
     <!-- HEADER PLAYER -->
     <div class="accessibilityPlayerAudioHeader">
@@ -55,7 +54,7 @@ export const audioPlayerHtml = `
             <input type="range" min="0" max="1" value="0.5" step="0.1" />
         </div>
         <div style="display: flex; flex-direction: row; align-items: center;">
-            <a style="margin-right: 6px; color: inherit;" href="#">
+            <a style="margin-right: 6px; color: inherit;" href="#" id="audioPlayerPrev">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
                     class="bi bi-skip-start-fill" viewBox="0 0 16 16">
                     <path
@@ -63,15 +62,14 @@ export const audioPlayerHtml = `
                 </svg>
 
             </a>
-            <a style="margin-right: 6px; color: inherit;" href="#">
+            <a style="margin-right: 6px; color: inherit;" href="#" id="audioPlayerStartStop">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
                     class="bi bi-play-circle-fill" viewBox="0 0 16 16">
                     <path
                         d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                 </svg>
-
             </a>
-            <a style="margin-right: 12px; color: inherit;" href="#">
+            <a style="margin-right: 12px; color: inherit;" href="#" id="audioPlayerNext">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
                     class="bi bi-skip-end-fill" viewBox="0 0 16 16">
                     <path
@@ -90,8 +88,40 @@ export const audioPlayerHtml = `
     </div>
     <!-- FIM RODARÉ PLAYER -->
 </div>
-<!-- FIM AUDIO PLAYER -->
 `;
+
+let _audioPlayerTimeout;
+
+const _myTimer = () => {
+    console.log("exec");
+    speechSynthesis.pause();
+    speechSynthesis.resume();
+    _audioPlayerTimeout = setTimeout(_myTimer, 10000);
+};
+
+const _onPauseOrEnd = () => {
+    clearTimeout(_audioPlayerTimeout);
+    let btnStartStop = document.getElementById("audioPlayerStartStop");
+    if (!!btnStartStop) {
+        btnStartStop.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" id="playAudio" viewBox="0 0 16 16">
+        <path
+        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+    </svg>
+    `;
+    }
+};
+
+const _onStartOrPlay = () => {
+    console.log("start");
+    _audioPlayerTimeout = setTimeout(_myTimer, 10000);
+    let btnStartStop = document.getElementById("audioPlayerStartStop");
+    btnStartStop.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" id="pauseAudio" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>
+    </svg>
+    `;
+};
 
 /**
  *
@@ -100,8 +130,72 @@ export const audioPlayerHtml = `
 export const toogleAudioPlayer = (isShow) => {
     if (isShow) {
         document.body.insertAdjacentHTML("beforeend", audioPlayerHtml);
+
+        let btnStartStop = document.getElementById("audioPlayerStartStop");
+        let btnNext = document.getElementById("audioPlayerNext");
+        let btnPrev = document.getElementById("audioPlayerPrev");
+
+        /**
+         * Play / Stop
+         */
+        btnStartStop.addEventListener("click", (event) => {
+            event.preventDefault();
+            let isPlay = event.target.parentElement.id === "playAudio";
+            if (isPlay) {
+                speechSynthesis.resume();
+                _onStartOrPlay();
+            } else {
+                speechSynthesis.pause();
+                _onPauseOrEnd();
+            }
+        });
+
+        btnNext.addEventListener("click", () => {
+            let allTexts = document.querySelectorAll(".hasText");
+
+            for (const [indexText, text] of allTexts.entries()) {
+                if (text.hasAttribute("playing") && !!allTexts[indexText + 1]) {
+                    allTexts[indexText + 1].click();
+                    console.log('proximo');
+                    break;
+                }
+            }
+        });
+
+        btnPrev.addEventListener("click", () => {
+            let allTexts = document.querySelectorAll(".hasText");
+
+            for (const [indexText, text] of allTexts.entries()) {
+                if (text.hasAttribute("playing") && !!allTexts[indexText - 1]) {
+                    allTexts[indexText - 1].click();
+                    console.log('anterior');
+                    break;
+                }
+            }
+        });
     } else {
         document.getElementById("accessibilityAudioPlayer").remove();
     }
+};
 
+/**
+ *
+ * @param {SpeechSynthesisUtterance} speechInstance
+ */
+export const enableSpeechEvents = (speechInstance) => {
+    speechInstance.onstart = () => {
+        clearTimeout(_audioPlayerTimeout);
+        _onStartOrPlay();
+    };
+
+    speechInstance.onend = () => {
+        // let textPlaying = document.querySelector(`.hasText[playing="true"]`);
+
+        // textPlaying.removeAttribute("playing");
+        _onPauseOrEnd();
+    };
+
+    speechInstance.onerror = (e) => {
+        console.log(e);
+    };
 };
