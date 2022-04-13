7d8d5e89698a422f8fa9473f97d801108366bd9e
Mamura <mamura@leoa.com.br>
2022-04-13 11:09:41 -0300

    ponteiro do rato

--- src/bigCursorBlack/bigCursorBlack.mjs
+++ src/bigCursorBlack/bigCursorBlack.mjs
@@ -1,23 +1,21 @@
 export default function bigCursorBlack(self, destroy) {
-  if (self.sessionState.bigCursorWhite) {
-    self.menuInterface.bigCursorWhite(true);
-  }
-  if (destroy) {
-    self.html.classList.remove("_access_cursor_black");
-    document
-      .querySelector('._access-menu [data-access-action="bigCursorBlack"]')
-      .classList.remove("active");
-    self.initialValues.bigCursorBlack = false;
-    self.sessionState.bigCursorBlack = false;
-    self.onChange(true);
-    return;
-  }
+    if (self.sessionState.bigCursorWhite) {
+        self.menuInterface.bigCursorWhite(true);
+    }
+  
+    if (destroy) {
+        $('html').removeClass("_access_cursor_black");
+        $('._access-menu [data-access-action="bigCursorBlack"]').removeClass("active");
+        self.initialValues.bigCursorBlack = false;
+        self.sessionState.bigCursorBlack = false;
+        self.onChange(true);
+    
+        return;
+    }
 
-  document
-    .querySelector('._access-menu [data-access-action="bigCursorBlack"]')
-    .classList.toggle("active");
-  self.initialValues.bigCursorBlack = !self.initialValues.bigCursorBlack;
-  self.sessionState.bigCursorBlack = self.initialValues.bigCursorBlack;
-  self.onChange(true);
-  self.html.classList.toggle("_access_cursor_black");
+    $('._access-menu [data-access-action="bigCursorBlack"]').toggleClass("active");
+    self.initialValues.bigCursorBlack = !self.initialValues.bigCursorBlack;
+    self.sessionState.bigCursorBlack = self.initialValues.bigCursorBlack;
+    self.onChange(true);
+    $('html').toggleClass("_access_cursor_black");
 }
